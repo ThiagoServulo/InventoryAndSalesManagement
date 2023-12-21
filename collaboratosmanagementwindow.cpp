4a3c67c048359e1f0d20dfe900e10557362475b5
ThiagoServulo <thiagoservulog@gmail.com>
2023-12-21 07:56:06 -0300

    Create collaborators management tab layout

--- collaboratosmanagementwindow.cpp
+++ collaboratosmanagementwindow.cpp
@@ -1,14 +1,32 @@
 #include "collaboratosmanagementwindow.h"
 #include "ui_collaboratosmanagementwindow.h"
+#include <QMessageBox>
 
 CollaboratosManagementWindow::CollaboratosManagementWindow(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::CollaboratosManagementWindow)
 {
     ui->setupUi(this);
+
+    if(!dbConnection.open())
+    {
+        QMessageBox::warning(this, "Error", "Unable to connect database");
+    }
+
+    ui->comboBox_nc_accessType->addItem("A");
+    ui->comboBox_nc_accessType->addItem("B");
+    ui->lineEdit_nc_password->setEchoMode(QLineEdit::Password);
+    ui->lineEdit_nc_confirmPassword->setEchoMode(QLineEdit::Password);
+    ui->lineEdit_nc_name->setFocus();
 }
 
 CollaboratosManagementWindow::~CollaboratosManagementWindow()
 {
     delete ui;
 }
+
+void CollaboratosManagementWindow::on_pushButton_nc_save_clicked()
+{
+
+}
+
--- collaboratosmanagementwindow.h
+++ collaboratosmanagementwindow.h
@@ -2,6 +2,7 @@
 #define COLLABORATOSMANAGEMENTWINDOW_H
 
 #include <QDialog>
+#include "databaseconnection.h"
 
 namespace Ui {
 class CollaboratosManagementWindow;
@@ -12,8 +13,15 @@ class CollaboratosManagementWindow : public QDialog
     Q_OBJECT
 
 public:
+    // Properties
+    DatabaseConnection dbConnection;
+
+    // Functions
     explicit CollaboratosManagementWindow(QWidget *parent = nullptr);
     ~CollaboratosManagementWindow();
+
+private slots:
+    void on_pushButton_nc_save_clicked();
 
 private:
     Ui::CollaboratosManagementWindow *ui;

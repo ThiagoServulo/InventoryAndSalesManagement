8a401b519da9ded8aca0ce811cc2b90f63233b03
ThiagoServulo <thiagoservulog@gmail.com>
2024-01-11 07:57:35 -0300

    Remove id field when update a product information

--- inventorymanagementwindow.cpp
+++ inventorymanagementwindow.cpp
@@ -10,36 +10,33 @@ InventoryManagementWindow::InventoryManagementWindow(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::InventoryManagementWindow)
 {
-    ui->setupUi(this);
+    Utilities utilities;
 
-    if(!dbConnection.open())
-    {
-        QMessageBox::warning(this, "Error", "Unable to connect database");
-    }
+    ui->setupUi(this);
 
     ui->tabWidget->setCurrentIndex(0);
     ui->tableWidget_inventoryManagement->setColumnCount(2);
     ui->tableWidget_inventoryManagement->setColumnWidth(0, 100);
     ui->tableWidget_inventoryManagement->setColumnWidth(1, 200);
     ui->tableWidget_inventoryManagement->setHorizontalHeaderLabels({"Id", "Description"});
-    ui->tableWidget_inventoryManagement->setStyleSheet("QTableView {selection-background-color: red}");
-    ui->tableWidget_inventoryManagement->setEditTriggers(QAbstractItemView::NoEditTriggers);
-    ui->tableWidget_inventoryManagement->setSelectionBehavior(QAbstractItemView::SelectRows);
-    ui->tableWidget_inventoryManagement->verticalHeader()->setVisible(false);
+    utilities.TableWidgetBasicConfigurations(ui->tableWidget_inventoryManagement);
 
-    QValidator *validatorFloat = new QRegularExpressionValidator(QRegularExpression("[0-9]*\\.?[0-9]*"));
-    ui->lineEdit_newProduct_salePrice->setValidator(validatorFloat);
-    ui->lineEdit_newProduct_purchasePrice->setValidator(validatorFloat);
-    ui->lineEdit_inventoryManagement_salePrice->setValidator(validatorFloat);
-    ui->lineEdit_inventoryManagement_purchasePrice->setValidator(validatorFloat);
+    // Configure regex to int fields
+    utilities.ConfigureRegexLineEdit(ui->lineEdit_newProduct_salePrice, 3);
+    utilities.ConfigureRegexLineEdit(ui->lineEdit_newProduct_purchasePrice, 3);
+    utilities.ConfigureRegexLineEdit(ui->lineEdit_inventoryManagement_salePrice, 3);
+    utilities.ConfigureRegexLineEdit(ui->lineEdit_inventoryManagement_purchasePrice, 3);
 
-    QValidator *validatorInt = new QRegularExpressionValidator(QRegularExpression("[0-9]*"));
-    ui->lineEdit_newProduct_quantity->setValidator(validatorInt);
-    ui->lineEdit_newProduct_supplier->setValidator(validatorInt);
-    ui->lineEdit_newProduct_id->setValidator(validatorInt);
-    ui->lineEdit_inventoryManagement_quantity->setValidator(validatorInt);
-    ui->lineEdit_inventoryManagement_supplier->setValidator(validatorInt);
-    ui->lineEdit_inventoryManagement_id->setValidator(validatorInt);
+    // Configure regex to float fields
+    utilities.ConfigureRegexLineEdit(ui->lineEdit_newProduct_quantity, 2);
+    utilities.ConfigureRegexLineEdit(ui->lineEdit_newProduct_supplier, 2);
+    utilities.ConfigureRegexLineEdit(ui->lineEdit_newProduct_id, 2);
+    utilities.ConfigureRegexLineEdit(ui->lineEdit_inventoryManagement_quantity, 2);
+    utilities.ConfigureRegexLineEdit(ui->lineEdit_inventoryManagement_supplier, 2);
+
+    // Configure regex to string fields
+    utilities.ConfigureRegexLineEdit(ui->lineEdit_newProduct_description, 1);
+    utilities.ConfigureRegexLineEdit(ui->lineEdit_inventoryManagement_description, 1);
 }
 
 InventoryManagementWindow::~InventoryManagementWindow()
@@ -50,6 +47,11 @@ InventoryManagementWindow::~InventoryManagementWindow()
 
 void InventoryManagementWindow::on_pushButton_newProduct_save_clicked()
 {
+    if(!dbConnection.open())
+    {
+        QMessageBox::warning(this, "Error", "Unable to connect database");
+    }
+
     int id = ui->lineEdit_newProduct_id->text().toInt();
     QString description = ui->lineEdit_newProduct_description->text();
     int id_supplier = ui->lineEdit_newProduct_supplier->text().toInt();
@@ -99,6 +101,11 @@ void InventoryManagementWindow::on_tabWidget_currentChanged(int index)
 
 void InventoryManagementWindow::UpdateIMTableWidget()
 {
+    if(!dbConnection.open())
+    {
+        QMessageBox::warning(this, "Error", "Unable to connect database");
+    }
+
     QSqlQuery query;
     query.prepare("SELECT id, description FROM tb_inventory ORDER BY id");
     Utilities utilities;
@@ -110,13 +117,17 @@ void InventoryManagementWindow::UpdateIMTableWidget()
 
 void InventoryManagementWindow::on_tableWidget_inventoryManagement_itemSelectionChanged()
 {
+    if(!dbConnection.open())
+    {
+        QMessageBox::warning(this, "Error", "Unable to connect database");
+    }
+
     int id = ui->tableWidget_inventoryManagement->item(ui->tableWidget_inventoryManagement->currentRow(), 0)->text().toInt();
     QSqlQuery query;
     query.prepare("SELECT * FROM tb_inventory WHERE id = " + QString::number(id));
     if(query.exec())
     {
         query.first();
-        ui->lineEdit_inventoryManagement_id->setText(query.value(0).toString());
         ui->lineEdit_inventoryManagement_description->setText(query.value(1).toString());
         ui->lineEdit_inventoryManagement_supplier->setText(query.value(2).toString());
         ui->lineEdit_inventoryManagement_quantity->setText(query.value(3).toString());
@@ -131,14 +142,18 @@ void InventoryManagementWindow::on_tableWidget_inventoryManagement_itemSelection
 
 void InventoryManagementWindow::on_pushButton_inventoryManagement_save_clicked()
 {
-    if(ui->lineEdit_inventoryManagement_id->text() == "")
+    if(ui->lineEdit_inventoryManagement_description->text() == "")
     {
         QMessageBox::warning(this, "Error", "Select a product");
     }
     else
     {
-        int idNew = ui->lineEdit_inventoryManagement_id->text().toInt();
-        int idOld = ui->tableWidget_inventoryManagement->item(ui->tableWidget_inventoryManagement->currentRow(), 0)->text().toInt();
+        if(!dbConnection.open())
+        {
+            QMessageBox::warning(this, "Error", "Unable to connect database");
+        }
+
+        int id = ui->tableWidget_inventoryManagement->item(ui->tableWidget_inventoryManagement->currentRow(), 0)->text().toInt();
         QString description = ui->lineEdit_inventoryManagement_description->text();
         int id_supplier = ui->lineEdit_inventoryManagement_supplier->text().toInt();
         float purchasePrice = ui->lineEdit_inventoryManagement_purchasePrice->text().toFloat();
@@ -146,10 +161,10 @@ void InventoryManagementWindow::on_pushButton_inventoryManagement_save_clicked()
         int quantity = ui->lineEdit_inventoryManagement_quantity->text().toInt();
 
         QSqlQuery query;
-        query.prepare("UPDATE tb_inventory SET id = " + QString::number(idNew) + ", description = '" + description +
-                      "', supplier = " + QString::number(id_supplier) + ", quantity = " + QString::number(quantity) +
-                      ", purchase_price = " + QString::number(purchasePrice) + ", sale_price = " + QString::number(salePrice) +
-                      " WHERE id = " + QString::number(idOld));
+        query.prepare("UPDATE tb_inventory SET description = '" + description + "', supplier = " +
+                      QString::number(id_supplier) + ", quantity = " + QString::number(quantity) +
+                      ", purchase_price = " + QString::number(purchasePrice) + ", sale_price = " +
+                      QString::number(salePrice) + " WHERE id = " + QString::number(id));
 
         if(query.exec())
         {
@@ -165,6 +180,11 @@ void InventoryManagementWindow::on_pushButton_inventoryManagement_save_clicked()
 
 void InventoryManagementWindow::on_pushButton_inventoryManagement_remove_clicked()
 {
+    if(!dbConnection.open())
+    {
+        QMessageBox::warning(this, "Error", "Unable to connect database");
+    }
+
     QMessageBox::StandardButton button = QMessageBox::question(this, "Remove", "Do you want to remove this product?", QMessageBox::Yes | QMessageBox::No);
     if(button == QMessageBox::Yes)
     {
@@ -174,7 +194,6 @@ void InventoryManagementWindow::on_pushButton_inventoryManagement_remove_clicked
         if(query.exec())
         {
             ui->tableWidget_inventoryManagement->removeRow(ui->tableWidget_inventoryManagement->currentRow());
-            ui->lineEdit_inventoryManagement_id->clear();
             ui->lineEdit_inventoryManagement_description->clear();
             ui->lineEdit_inventoryManagement_supplier->clear();
             ui->lineEdit_inventoryManagement_quantity->clear();
@@ -191,8 +210,13 @@ void InventoryManagementWindow::on_pushButton_inventoryManagement_remove_clicked
 
 void InventoryManagementWindow::on_pushButton_inventoryManagement_search_clicked()
 {
+    if(!dbConnection.open())
+    {
+        QMessageBox::warning(this, "Error", "Unable to connect database");
+    }
+
     QSqlQuery query;
-    if(ui->lineEdit_im_filter->text() == "")
+    if(ui->lineEdit_inventoryManagement_filter->text() == "")
     {
         if(ui->radioButton_inventoryManagement_id->isChecked())
         {
@@ -207,15 +231,15 @@ void InventoryManagementWindow::on_pushButton_inventoryManagement_search_clicked
     {
         if(ui->radioButton_inventoryManagement_id->isChecked())
         {
-            query.prepare("SELECT id, description FROM tb_inventory WHERE id = " + ui->lineEdit_im_filter->text());
+            query.prepare("SELECT id, description FROM tb_inventory WHERE id = " + ui->lineEdit_inventoryManagement_filter->text());
         }
         else
         {
-            query.prepare("SELECT id, description FROM tb_inventory WHERE description LIKE '%" + ui->lineEdit_im_filter->text() + "%'");
+            query.prepare("SELECT id, description FROM tb_inventory WHERE description LIKE '%" + ui->lineEdit_inventoryManagement_filter->text() + "%'");
         }
     }
 
-    ui->lineEdit_im_filter->clear();
+    ui->lineEdit_inventoryManagement_filter->clear();
 
     if(query.exec())
     {
